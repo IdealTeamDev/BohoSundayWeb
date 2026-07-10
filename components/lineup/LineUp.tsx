0fd23be2b982563d8fb81e513620fa18c47514c4
AleGuir <alejaguirales26@gmail.com>
2026-07-10 14:33:37 -0500

    Ajustes djs y form Pao QR

--- components/lineup/LineUp.tsx
+++ components/lineup/LineUp.tsx
@@ -5,12 +5,12 @@ import { useParams } from 'next/navigation';
 import { translations } from '@/data/translations';
  
 const djs = [
-  { id: 1, name: 'ANTDOT',       photo: '/images/lineup/ANTIDOT.png' },
-  { id: 2, name: '2 NOMADS',     photo: '/images/lineup/NOMADS.png' },
-  { id: 3, name: 'ROLAND CLARK', photo: '/images/lineup/ROLAND.png' },
-  { id: 4, name: 'NICOLAII',     photo: '/images/lineup/NICOLA.png' },
-  { id: 5, name: 'KUBHA',        photo: '/images/lineup/KUBHA.png' },
-  { id: 6, name: 'AMOSER',       photo: '/images/lineup/AMOSER.png' },
+  { id: 1, name: 'MALÓNE MOREZ', photo: '/images/lineup/ANTIDOT.png' },
+  { id: 2, name: 'BARON (FR)',   photo: '/images/lineup/NOMADS.png' },
+  { id: 3, name: 'SEVENKEY', photo: '/images/lineup/ROLAND.png' },
+  { id: 4, name: 'BENJP',     photo: '/images/lineup/NICOLA.png' },
+  { id: 5, name: 'VLAIR',        photo: '/images/lineup/KUBHA.png' },
+  { id: 6, name: 'ELISABETH MOON',       photo: '/images/lineup/AMOSER.png' },
 ];
  
 export default function LineUp() {
